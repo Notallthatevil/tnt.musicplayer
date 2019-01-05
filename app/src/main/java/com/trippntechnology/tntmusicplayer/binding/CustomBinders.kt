71a41b4e8430cc83c2a752b477a460347ec7a773
Nate <nate.tripp13448@gmail.com>
2019-01-05 16:14:14 -0700

    Added image loading with Glide

--- app/src/main/java/com/trippntechnology/tntmusicplayer/binding/CustomBinders.kt
+++ app/src/main/java/com/trippntechnology/tntmusicplayer/binding/CustomBinders.kt
@@ -3,29 +3,36 @@ package com.trippntechnology.tntmusicplayer.binding
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.trippntechnology.tntmusicplayer.R
 import java.util.concurrent.TimeUnit
 
-object CustomBinders{
+object CustomBinders {
 
     @JvmStatic
     @BindingAdapter("setImage")
-    fun setImage(imageView: ImageView,image:ByteArray?){
-        if (image==null){
-            imageView.setImageResource(R.mipmap.ic_launcher)
-        }
-        //Temp
-        imageView.setImageResource(R.mipmap.ic_launcher)
+    fun setImage(imageView: ImageView, image: ByteArray?) {
+        val requestOptions = RequestOptions()
+            .placeholder(R.drawable.ic_launcher_background)
+            .transforms(RoundedCorners(12))
+        Glide.with(imageView)
+            .load(image)
+            .apply(requestOptions)
+            .transition(withCrossFade())
+            .into(imageView)
     }
 
     @JvmStatic
     @BindingAdapter("setDuration")
-    fun setDuration(textView: TextView,duration:Long){
-        val mmss = String.format("%02d:%02d",
+    fun setDuration(textView: TextView, duration: Long) {
+        val mmss = String.format(
+            "%d:%02d",
             TimeUnit.MILLISECONDS.toMinutes(duration) % TimeUnit.HOURS.toMinutes(1),
-            TimeUnit.MILLISECONDS.toSeconds(duration) % TimeUnit.MINUTES.toSeconds(1))
+            TimeUnit.MILLISECONDS.toSeconds(duration) % TimeUnit.MINUTES.toSeconds(1)
+        )
         textView.text = mmss
     }
 
