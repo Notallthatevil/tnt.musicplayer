67b83f6845b1daa256d7656de0406d5a83b8806c
Nate <nate.tripp13448@gmail.com>
2019-04-11 17:15:34 -0600

    Updated gradle version and added player lib library structuring

---
+++ app/src/main/cpp/tntplayer/TNTAudioPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nate on 3/27/2019.
+//
+
+#include "TNTAudioPlayer.h"
---
+++ app/src/main/cpp/tntplayer/TNTAudioPlayer.h
@@ -0,0 +1,14 @@
+//
+// Created by Nate on 3/27/2019.
+//
+
+#ifndef TNT_MUSICPLAYER_TNTAUDIOPLAYER_H
+#define TNT_MUSICPLAYER_TNTAUDIOPLAYER_H
+
+
+class TNTAudioPlayer {
+
+};
+
+
+#endif //TNT_MUSICPLAYER_TNTAUDIOPLAYER_H
